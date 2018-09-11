c1d1d7cdf64e36284d74cbc05fdfb842b7177a7f
amir.suhail <amir.suhail@accenture.com>
2018-09-12 01:38:17 +0530

    GET /todos API/test added

--- server/server.js
+++ server/server.js
@@ -5,22 +5,40 @@ const {mongoose} = require ('./db/mongoose');
 const {User} = require ('./db/model/user');
 const {Todo} = require ('./db/model/todos');
 
-
+// ========================= Express Middleware ================================
 
 var app = express();
 app.use(bodyParser.json());
 
+// ========================= API Routes ========================================
+
+    // ===================== POST /todos =======================================
+
 app.post('/todos',(req,res) => {
     let todo = new Todo({text:req.body.text});
     todo.save().then((doc) => {
-        console.log('To added!!');
         res.send(doc);
     },(err)=>{
-        console.log('Not updated!!');
         res.status(400).send(err);
     });
 
 });
+
+    // ===================== GET /todos =======================================
+
+app.get('/todos',(req,res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send(todos);
+
+    },(err) => {
+
+        res.status(400).send(err);
+    })
+})
+
+// ========================= STARTING SERVER ON PORT 5001 =======================================
 
 app.listen(5001,()=>{
     console.log('Server starting at port 5001 !!')
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,22 @@ const request = require ('supertest');
 const {app} = require ('./../server');
 const {Todo} = require ('./../db/model/todos');
 
+var todos = [{
+    text: 'Sample test Todo 1'
+},{
+    text: 'Sample test Todo 2'
+}];
+
+// ================================== will Run before tests are called ==================
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.deleteMany({completedAt: null}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
+
+
+// ================================== POST /todos test cases =============================
 
 describe ('POST /todos', () => {
 
@@ -18,7 +31,7 @@ describe ('POST /todos', () => {
         .send({text})
         .expect(200)
         .expect((res) => {
-            console.log(res.body.text);
+
             expect(res.body.text).toBe(text);
 
         }).end((err,res) => {
@@ -26,13 +39,49 @@ describe ('POST /todos', () => {
             if(err)
                 return done(err);
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
             }).catch((e) => done(e));
         });
 
+    });
+
+    it('should not add invalid data',(done) => {
+
+        request(app)
+        .post('/todos')
+        .send({})
+        .expect(400)
+        .end((err,res) => {
+            if(err)
+                return done(err);
+
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(2);
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+});
+
+// ================================== GET /todos test cases =============================
+
+describe('GET /todos', () => {
+
+    it('should return all todos',(done) => {
+
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+
+            expect(res.body.length).toBe(2);
+
+        })
+        .end(done);
     });
 
 });
