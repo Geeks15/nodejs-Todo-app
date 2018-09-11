934cd62a1d17298bb7ba41c7d1cf3d69b859f53d
amir suhail <aamirsohail.as@gmail.com>
2018-09-11 19:57:53 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+
+const {mongoose} = require ('./db/mongoose');
+const {User} = require ('./db/model/user');
+const {Todo} = require ('./db/model/todos');
+
+
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/todos',(req,res) => {
+    let todo = new Todo({text:req.body.text});
+    todo.save().then((doc) => {
+        console.log('To added!!');
+        res.send(doc);
+    },(err)=>{
+        console.log('Not updated!!');
+        res.status(400).send(err);
+    });
+
+});
+
+app.listen(5001,()=>{
+    console.log('Server starting at port 5001 !!')
+})
+
+// let newUser = new User({email: 'amir.suhail@accenture.com'});
+// newUser.save().then((doc)=>{console.log('User Added!!')},(err)=>{console.log(`User can't be added!!`)});
